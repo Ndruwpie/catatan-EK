b8082c69c486ae478935e6c4b6f539709d67db50
ndruw <ndruwpie@gmail.com>
2026-01-06 00:42:44 +0700

    update tampilan dan perbaikan url

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+// eslint-disable-next-line no-unused-vars
+import tailwindcss from '@tailwindcss/vite' // <-- Pastikan baris ini ada
+
 
 // https://vite.dev/config/
 export default defineConfig({
